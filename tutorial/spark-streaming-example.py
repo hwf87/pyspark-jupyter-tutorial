bd9ea3473942875d8f1bc2af094aa9c6e93785cd
jackyfu <jackyfu1995@gmail.com>
2023-02-23 17:17:02 +0800

    add spark-streaming example

---
+++ tutorial/spark-streaming-example.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# docker exec -it -u root {container_id} bash
+# sudo apt update
+# sudo apt install netcat
+
+# nc -lk 9999
+# /usr/local/spark/bin/spark-submit streaming-example.py localhost 9999
+
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+
+# Create a local StreamingContext with two working thread and batch interval of 1 second
+sc = SparkContext("local[2]", "NetworkWordCount")
+ssc = StreamingContext(sc, 10)
+
+# Create a DStream that will connect to hostname:port, like localhost:9999
+lines = ssc.socketTextStream("localhost", 9999)
+
+# Split each line into words
+words = lines.flatMap(lambda line: line.split(" "))
+
+# Count each word in each batch
+pairs = words.map(lambda word: (word, 1))
+wordCounts = pairs.reduceByKey(lambda x, y: x + y)
+
+# Print the first ten elements of each RDD generated in this DStream to the console
+wordCounts.pprint()
+
+ssc.start()             # Start the computation
+ssc.awaitTermination()  # Wait for the computation to terminate
